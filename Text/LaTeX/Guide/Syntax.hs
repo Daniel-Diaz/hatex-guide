ed856b61038e457b00358cc3887939869979ce16
Ramiro <ramiro.sgaray@gmail.com>
2018-09-15 19:44:27 -0300

    Add support for older versions of base

--- Text/LaTeX/Guide/Syntax.hs
+++ Text/LaTeX/Guide/Syntax.hs
@@ -1,5 +1,6 @@
 
 {-# LANGUAGE NoImplicitPrelude, OverloadedStrings #-}
+{-# LANGUAGE CPP #-}
 {-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
 
 module Text.LaTeX.Guide.Syntax (
@@ -17,7 +18,12 @@ import Data.Text
 import Data.Text.IO hiding (putStr)
 import Data.Int
 import Data.Bool
-import Prelude (Eq(..),Show(..),FilePath,Enum,Semigroup)
+import Prelude (Eq(..),Show(..),FilePath,Enum)
+
+#if MIN_VERSION_base(4,11,0)
+import Prelude (Semigroup)
+#endif
+
 import Data.Function
 import Control.Monad
 import qualified Data.List as L
@@ -74,6 +80,8 @@ data Syntax =
  | Empty
    deriving Show
 
+#if MIN_VERSION_base(4,11,0)
+
 instance Semigroup Syntax where
  Empty <> x = x
  x <> Empty = x
@@ -81,6 +89,16 @@ instance Semigroup Syntax where
 
 instance Monoid Syntax where
  mempty = Empty
+
+#else
+
+instance Monoid Syntax where
+ Empty `mappend` x = x
+ x `mappend` Empty = x
+ x `mappend` y = Append x y
+ mempty = Empty
+
+#endif
 
 -- Printer
 
